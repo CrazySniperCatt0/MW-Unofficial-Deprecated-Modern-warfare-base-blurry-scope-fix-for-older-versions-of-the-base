f4db70bd3095318c24e589ae8de159e8f1599190
CrazySniperCatt0 <114126433+CrazySniperCatt0@users.noreply.github.com>
2022-10-20 19:00:51 -0500

    Delete uniquenametoavoidstupidaddonconflicts.lua

--- modern_warfare_base_automatic_blur_remover_2864865546/lua/autorun/client/uniquenametoavoidstupidaddonconflicts.lua
+++
@@ -1,4 +0,0 @@
-RunConsoleCommand("breakprops_active", "1")
-RunConsoleCommand("breakprops_change_map_doors", "1")
-RunConsoleCommand("breakprops_change_map_props", "1")
-RunConsoleCommand("breakprops_change_playerspawn_prop", "1")
